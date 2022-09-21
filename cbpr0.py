8c5a8c475da4d84678fec523c60fde826a03b633
oskuH <104070288+oskuH@users.noreply.github.com>
2022-09-21 17:48:39 +0300

    Update cbpr0.py

--- cbpr0.py
+++ cbpr0.py
@@ -13,13 +13,15 @@ gran = 3600
 #fetch raw data:
 c = cbpro.PublicClient()
 rates = c.get_product_historic_rates(product_id=currency_pair, start=start_date, end=end_date, granularity=gran)
-
-#create a dataframe with timestamp as index and with change of price added:
 df = pd.DataFrame(rates, columns=['time', 'low', 'high', 'open', 'close', 'volume'])
+
+#check if settings are valid:
 if df.empty == True:
     print("Too many data points for a single request!")
     print("Try again after editing dates and/or granularity.")
     exit()
+    
+#create a dataframe with timestamp as index and with change of price added:
 df.index = pd.to_datetime(df['time'],unit='s')
 df = df.drop(columns='time')
 df = df.sort_values('time')
